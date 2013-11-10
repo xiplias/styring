fb7aeb1227bac8d990f1882b3a6c6cba13ca8154
Anders Hansen <xiplias@gmail.com>
2013-11-10 20:38:11 +0100

    Adding story name to task

--- client/views/task/task.js
+++ client/views/task/task.js
@@ -1,6 +1,9 @@
 Template.task.helpers({
   componentName: function () {
     return FormHelper.component(this.componentId).name;
+  },
+  storyName: function () {
+    return Stories.findOne({_id: this.storyId}).title;
   }
 });
 
