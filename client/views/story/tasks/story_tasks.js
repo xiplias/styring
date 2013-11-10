635b6d806efaf1aee5f54375b84c3b434fd54608
Anders Hansen <xiplias@gmail.com>
2013-11-10 20:09:30 +0100

    Move task form into own template

--- client/views/story/tasks/story_tasks.js
+++ client/views/story/tasks/story_tasks.js
@@ -1,41 +1,6 @@
 Template.storyTasks.helpers({
-  tasksCollection: function () {
-    return Tasks;
-  },
   tasks: function () {
     return Tasks.find({storyId: FormHelper.currentStoryId(), projectId: FormHelper.currentProject()._id });
-  },
-  pointsOptions: function () {
-    return [
-      { label: "0", value: "0"},
-      { label: "1", value: "1"},
-      { label: "2", value: "2"},
-      { label: "3", value: "3"},
-      { label: "5", value: "5"},
-      { label: "7", value: "7"}
-    ];
-  },
-  assignOptions: function () {
-    var users = Meteor.users.find({}).fetch().map(function (user) {
-      return { label: user.profile.name, value: user._id }
-    });
-    console.log(users);
-    return users;
-  },
-  selectedTask: function () {
-    return Session.get('selectedTask');
-  },
-  componentOptions: function () {
-    return FormHelper.componentOptions(true);
-  },
-  formState: function () {
-    return Session.get('selectedTask') ? 'update' : 'insert';
-  },
-  storyId: function () {
-    return FormHelper.currentStoryId();
-  },
-  projectId: function () {
-    return FormHelper.currentProject()._id
   }
 });
 
---
+++ client/views/task/form/task_form.js
@@ -0,0 +1,37 @@
+Template.taskForm.helpers({
+  tasksCollection: function () {
+    return Tasks;
+  },
+  pointsOptions: function () {
+    return [
+      { label: "0", value: "0"},
+      { label: "1", value: "1"},
+      { label: "2", value: "2"},
+      { label: "3", value: "3"},
+      { label: "5", value: "5"},
+      { label: "7", value: "7"}
+    ];
+  },
+  assignOptions: function () {
+    var users = Meteor.users.find({}).fetch().map(function (user) {
+      return { label: user.profile.name, value: user._id }
+    });
+
+    return users;
+  },
+  selectedTask: function () {
+    return Session.get('selectedTask');
+  },
+  componentOptions: function () {
+    return FormHelper.componentOptions(true);
+  },
+  formState: function () {
+    return Session.get('selectedTask') ? 'update' : 'insert';
+  },
+  storyId: function () {
+    return FormHelper.currentStoryId();
+  },
+  projectId: function () {
+    return FormHelper.currentProject()._id;
+  }
+});
