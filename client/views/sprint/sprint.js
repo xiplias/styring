09f92edb1d61af4fef722b185d89bb2f37cba0c0
Anders Hansen <xiplias@gmail.com>
2013-11-23 23:50:58 +0100

    Fixed up sprint after data structure change

--- client/views/sprint/sprint.js
+++ client/views/sprint/sprint.js
@@ -1,21 +1,25 @@
 Template.sprint.helpers({
   users: function () {
-    return Meteor.users.find();
+    return Meteor.users.find().fetch();
   },
   userTasks: function () {
-    var components  = Components.find({projectId: FormHelper.currentProject()._id});
+    var components  = Components.find({projectId: FormHelper.currentProject()._id}).fetch();
+    var user = this;
 
     // Get Stories from each component and use the our sort algoritm
     var componentStories = components.map(function (component) {
-      var stories = Stories.find({componentId: component._id}).fetch(),
-          sortedStories = StoriesLib.orderByOrderList(stories, component.storyOrder);
-      return sortedStories;
+      if (component.storyOrder) {
+        return _.chain(JSON.parse(component.storyOrder)).toArray().flatten().value();
+      } else {
+        return [];
+      }
     });
 
     var zippedStories = StoriesLib.mergeByZipper(componentStories);
 
     var sortedTasks = _.chain(zippedStories).map(function (story) {
-      var tasks = Tasks.find({storyId: story._id, assignId: this._id}).fetch();
+      console.log({storyId: story, assignId: user._id})
+      var tasks = Tasks.find({storyId: story, assignId: user._id}).fetch();
       console.log(tasks);
       return tasks;
     }).flatten().value();
